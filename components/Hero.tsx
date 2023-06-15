285e49b4e0729fa1b29bdae1e94a2b948d0b5149
jackbasso <jackbasso.r@gmail.com>
2023-06-14 22:24:29 -0600

    Car catalogue section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,10 +29,11 @@ const Hero = () => {
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill
           className='object-contain' />
-          <div className='hero__image-overlay'/>
+        </div>
+        <div className='hero__image-overlay'/>
         </div>
       </div>
-    </div>
+    
   )
 }
 
