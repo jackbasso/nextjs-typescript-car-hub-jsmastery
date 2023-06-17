b6dac4d16c7c80884f252fc189f33a4c09b1ceb5
jackbasso <jackbasso.r@gmail.com>
2023-06-16 22:48:41 -0600

    search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -13,7 +13,7 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer  
           manufacturer={manufacturer}
-          setManufacturer=(setManufacturer)
+          setManufacturer={setManufacturer}
         />
       </div>
     </form>
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,16 +1,43 @@
 'use client'; 
 
-import { Combobox, Transition } from '@headlessui/react'
-import { SearchManufacturerProps } from '@/types'
-import React from 'react'
-59:34
-
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types';
+import Image from 'next/image';
+import { useState, Fragment } from 'react';
+1:03
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className='search-manufacturer'>SearchManufacturer
+    {/* headlessui */}
       <Combobox>
         <div className='relative w-full'>
+          <Combobox.Button className="absolute top-[14px]">
+            <Image 
+            src='/car-logo.svg'
+            width={20}
+            height={20}
+            className="ml-4"
+            alt="Car Logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input 
+            className="search-manufacturer__input"
+            placeholder='Volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
 
+          <Transition 
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery("")}
+            >
+
+          </Transition>
         </div>
       </Combobox>
     </div>
