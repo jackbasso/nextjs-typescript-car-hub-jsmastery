44ad61a88a2b61c987c9443bb20aad109d37f0b1
jackbasso <jackbasso.r@gmail.com>
2023-06-17 22:45:51 -0600

    Search filter manufacturers

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,9 +4,20 @@ import { Combobox, Transition } from '@headlessui/react';
 import { SearchManufacturerProps } from '@/types';
 import Image from 'next/image';
 import { useState, Fragment } from 'react';
-1:03
+
+import { manufacturers } from '@/constants';
+
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
+
+  const filteredManufacturers = 
+    query === ""
+    ? manufacturers
+    : manufacturers.filter((item) => (
+      item.toLowerCase()
+      .replace(/\s+/g, "")
+      .includes(query.toLowerCase().replace(/\s+/g, "")
+    )))
 
   return (
     <div className='search-manufacturer'>SearchManufacturer
@@ -36,7 +47,30 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
             leaveTo='opacity-0'
             afterLeave={() => setQuery("")}
             >
-
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  Create "{query}"
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) =>
+                      `relative search-manufacturer__option ${
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={item}
+                  >
+                    {item}
+                  </Combobox.Option>
+                )
+              ))}
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
