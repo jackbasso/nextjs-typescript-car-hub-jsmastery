f4acfa1114d41f44289cb153530ec00812a46806
jackbasso <jackbasso.r@gmail.com>
2023-06-18 21:37:03 -0600

    manufacturers search bar headlessui

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -4,7 +4,7 @@ import { Combobox, Transition } from '@headlessui/react';
 import { SearchManufacturerProps } from '@/types';
 import Image from 'next/image';
 import { useState, Fragment } from 'react';
-
+1:16
 import { manufacturers } from '@/constants';
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
@@ -22,7 +22,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
   return (
     <div className='search-manufacturer'>SearchManufacturer
     {/* headlessui */}
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className="absolute top-[14px]">
             <Image 
@@ -48,15 +48,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
             afterLeave={() => setQuery("")}
             >
             <Combobox.Options>
-              {filteredManufacturers.length === 0 && query !== "" ? (
-                <Combobox.Option
-                  value={query}
-                  className="search-manufacturer__option"
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ) : (
-                filteredManufacturers.map((item) => (
+                {filteredManufacturers.map((item) => (
                   <Combobox.Option
                     key={item}
                     className={({ active }) =>
@@ -66,10 +58,23 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
                     }
                     value={item}
                   >
-                    {item}
+                    {/* 112 */}
+                    {({ selected, active }) => ( 
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 )
-              ))}
+              )}
             </Combobox.Options>
           </Transition>
         </div>
