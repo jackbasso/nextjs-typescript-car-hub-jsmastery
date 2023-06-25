1490283b3a9f6a8af1a1cf8cae186e41b285c730
jackbasso <jackbasso.r@gmail.com>
2023-06-25 13:41:41 -0600

    utils calculateCarRent

--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,41 @@
-const url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars';
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'f8acc0fa5emshd5ef2f975f2e69fp100d6djsnfd08fe60fe91',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
 };
 
-try {
-	const response = await fetch(url, options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
+
+
+export async function fetchCars() {
+  //const { manufacturer, year, model, limit, fuel } = filters;
+
+  // Set the required headers for the API request
+  const headers = {
+    "X-RapidAPI-Key": 'f8acc0fa5emshd5ef2f975f2e69fp100d6djsnfd08fe60fe91',
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com"
+  }
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+    {
+      headers: headers,
+    }
+  );
+
+  // Parse the response as JSON
+  const result = await response.json();
+
+  return result;
 }
+
